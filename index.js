80e40310abafeaeb740c1038e67833e21b3a2bc0
Juho Ahonen <juho.ahonen2@gmail.com>
2024-04-15 16:15:02 +0300

    roomdetails wip

--- index.js
+++ index.js
@@ -29,17 +29,26 @@ const rooms = {
   room2: { user3: true, user4: true },
 };
 
+const emitRoomDetails = (roomId) => {
+  if (rooms[roomId]) {
+    const users = Object.keys(rooms[roomId]).map(username => ({ username }));
+    io.to(roomId).emit('roomDetails', { roomId, users });
+  }
+};
+
+
 io.on("connection", (socket) => {
   console.log("Client connected with ID:", socket.id);
 
   socket.on("createRoom", ({ username }) => {
     const roomId = Math.random().toString(36).substring(2, 9); // Generating a simple room ID
     rooms[roomId] = { [username]: true }; // Adding the room with the user to the in-memory storage
-
+  
     socket.join(roomId);
     console.log(`Room ${roomId} created and joined by ${username}`);
-
+  
     socket.emit("roomCreated", { roomId, username });
+    emitRoomDetails(roomId); // Emit room details after creation
   });
 
   socket.on("joinRoom", ({ roomId, username }) => {
@@ -47,14 +56,13 @@ io.on("connection", (socket) => {
       rooms[roomId][username] = true;
       socket.join(roomId);
       console.log(`${username} joined room ${roomId}`);
-
       socket.emit("joinRoomSuccess", { roomId, users: rooms[roomId] });
       io.to(roomId).emit("userJoined", username); // Notify others in the room
+      emitRoomDetails(roomId); // Emit updated room details
     } else {
       socket.emit("errorMessage", `Room ${roomId} does not exist.`);
     }
   });
-
   
 
   socket.on("leaveRoom", ({ roomId, username }) => {
@@ -62,14 +70,27 @@ io.on("connection", (socket) => {
       delete rooms[roomId][username];
       socket.leave(roomId);
       console.log(`${username} left room ${roomId}`);
-
       io.to(roomId).emit("userLeft", username); // Notify others in the room
+      emitRoomDetails(roomId); // Emit updated room details
     }
+  });
+
+  socket.on("disconnect", () => {
+    // Find and remove the user from rooms
+    Object.keys(rooms).forEach(roomId => {
+      if (rooms[roomId][socket.username]) {
+        delete rooms[roomId][socket.username];
+        io.to(roomId).emit("userLeft", socket.username);
+        emitRoomDetails(roomId); // Emit updated room details
+      }
+    });
+    console.log("Client disconnected", socket.id);
   });
 
   socket.on("chatMessage", ({ roomId, username, message }) => {
     io.to(roomId).emit("message", { username, text: message });
     console.log(`Message from ${username} in room ${roomId}: ${message}`);
+    console.log(rooms)
   });
 
   socket.on("disconnect", () => {
